c4e01f8efbd3b59424b70158535b237ef416ef44
abuccts <abuccts@gmail.com>
2015-12-22 01:10:24 +0800

    fix cascadelayout load bugs

--- js/widget.js
+++ js/widget.js
@@ -48,23 +48,24 @@ function ExtendedCarousel(rootEle, d, cols, param)
 		$('#' + lightSliderId).lightSlider({
 		    gallery: true,
 		    item: 1,
-			speed:500,
-            auto:true,
 		    loop:true,
 		    slideMargin: 0,
 		    thumbItem: 9
 		});
 	});
 
-	rootEle.masonry({
-		itemSelector: '.cascade-item',
-		isAnimated: true,
-		animationOptions: {
-			duration: 400
-		},
-		columnWidth: function(containerWidth) {
-			return containerWidth / cols;
-		}
+	rootEle.imagesLoaded(function(){
+		rootEle.masonry({
+			itemSelector: '.cascade-item',
+/*			isAnimated: true,
+			animationOptions: {
+				duration: 400
+			},
+*/
+			columnWidth: function(containerWidth) {
+				return containerWidth / cols;
+			}
+		});
 	});
 }
 
@@ -137,10 +138,10 @@ function TimeGlider(rootEle, param)
             "events":[
             ],
             "tags":{"mardigras":2,"chris":2,"arizona":2,"netscape":2,"flop":1}
-    
+
         }
     ];
-    
+
 	//TODO
 
 	///////////////////////////////////////////////d
@@ -154,13 +155,13 @@ function TimeGlider(rootEle, param)
         var etimestamp = $.myTime.ZCDateToUnix(end);
         var timestamp = (stimestamp + etimestamp)/2;
         var duration = (etimestamp-stimestamp)/30;
-        
+
         data_source[0].focus_date = $.myTime.UnixToDate(timestamp, true, 8);
         data_source[0].initial_zoom = 50;
 	}
 
 	//handles the layout and content of popup panel
-	// from top to bottom: 
+	// from top to bottom:
 	// 1. show date in title
 	// 2. show text descripiton on the top
 	// 3. show one image on the left
@@ -172,32 +173,32 @@ function TimeGlider(rootEle, param)
 	}
 
 	// get data from a json url
-	// past data will be cleaned and the view will be refreshed by the new data, 
+	// past data will be cleaned and the view will be refreshed by the new data,
 	// see if we can add some animation for such transition
 	// data is a list of [eid, imgurl, text, who list, where list, when list]
 	this.GetData = function(url)
 	{
-	   
+
         $.getJSON(url, function( data ) {
-            
+
             var starttime = 0;
             var endtime = 0;
-            
+
             //data_source = base_source;
             data_source[0].events = [];
-            
+
             $.each(data, function(index,obj){ //遍历json数据列
-            
+
                 var event = {};
                 event.id = obj[0];
-                
+
                 event.description = obj[2];
                 if(obj[2].length > 10){
                     event.title = obj[2].substr(0, 10)+"...";  //取前10个字符
                 }else{
                     event.title = obj[2].text;
                 }
-                
+
                 event.link = obj[1];
                 event.startdate = obj[5][0];
                 event.high_threshold = 60;
@@ -205,9 +206,9 @@ function TimeGlider(rootEle, param)
                 event.date_display = "ye";
                 event.icon = "circle_black.png";
                 event.image = "http://222.29.193.172:8006/lmHistoryTMMData/img/imgs/"+obj[1];
-                
+
                 data_source[0].events.push(event);
-                
+
                 if (starttime == 0 && endtime == 0){
                     starttime = obj[5][0];
 					endtime = obj[5][0];
@@ -223,11 +224,11 @@ function TimeGlider(rootEle, param)
             //alert(endtime);
             //调整设置参数
             _fitin(starttime, endtime);
-            
+
             rootEle.timeline({
-						
-				"min_zoom":1, 
-				"max_zoom":50, 
+
+				"min_zoom":1,
+				"max_zoom":50,
 				"timezone":"-06:00",
 				"icon_folder":"css/timeglider-1.0.3/icons/",
 				"data_source": data_source,
@@ -236,24 +237,24 @@ function TimeGlider(rootEle, param)
 				"mousewheel":"zoom", // zoom | pan | none
 				"constrain_to_data":false,
 				"image_lane_height":100,
-				"loaded":function () { 
+				"loaded":function () {
 					// loaded callback function
 				 }
-		
+
     			}).resizable({
-    					stop:function(){ 
+    					stop:function(){
     						// $(this).data("timeline").resize();
     					}
     			});
-       
-       
+
+
        /*
             //TODO
             if (!init){
                  tg1 = rootEle.timeline({
-						
-				"min_zoom":1, 
-				"max_zoom":50, 
+
+				"min_zoom":1,
+				"max_zoom":50,
 				"timezone":"-06:00",
 				"icon_folder":"css/timeglider-1.0.3/icons/",
 				"data_source": data_source,
@@ -262,12 +263,12 @@ function TimeGlider(rootEle, param)
 				"mousewheel":"zoom", // zoom | pan | none
 				"constrain_to_data":true,
 				"image_lane_height":100,
-				"loaded":function () { 
+				"loaded":function () {
 					// loaded callback function
 				 }
-		
+
     			}).resizable({
-    					stop:function(){ 
+    					stop:function(){
     						// $(this).data("timeline").resize();
     					}
     			});
@@ -322,8 +323,8 @@ function PopupPanel(rootEle, popupId, d, processFunc)
 					'<div class="modal-header">' +
 						'<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
 						'<h4 class="modal-title">Popup panel</h4>' +
-					'</div>' + 
-					processFunc(d) + 
+					'</div>' +
+					processFunc(d) +
 				'</div></div></div>');
 	rootEle.append($modal);
 }
@@ -347,7 +348,7 @@ function CascadeLayout(rootEle, d, cols, processFunc)
 	// 	}
 	// });
 
-	rootEle.masonry({
+/*	rootEle.masonry({
 		itemSelector: '.cascade-item',
 		// isAnimated: true,
 		// animationOptions: {
@@ -357,6 +358,7 @@ function CascadeLayout(rootEle, d, cols, processFunc)
 			return containerWidth / cols;
 		}
 	});
+*/
 
 	$.each(d, function(i, field) {
 
@@ -366,11 +368,28 @@ function CascadeLayout(rootEle, d, cols, processFunc)
 		// rootEle.append($cascadeItem).masonry('appended', $cascadeItem);
 
 		var cascadeItemHtml = processFunc(field);
-		rootEle.append($(cascadeItemHtml));
+		// $cascadeItem = $(cascadeItemHtml);
+		rootEle.append($(cascadeItemHtml));//.masonry('appended', $cascadeItem);
 	});
 
-	rootEle.imagesLoaded().progress(function(imgLoad, image){
+/*	rootEle.imagesLoaded().progress(function(imgLoad, image){
 		// console.log('loading');
 		rootEle.mansory('layout');
 	});
+	*/
+
+	rootEle.imagesLoaded(function(){
+		rootEle.masonry({
+			itemSelector: '.cascade-item',
+/*			isAnimated: true,
+			animationOptions: {
+				duration: 400
+			},
+*/
+			columnWidth: function(containerWidth) {
+				return containerWidth / cols;
+			}
+		});
+	});
+
 }
