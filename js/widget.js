08be00e44e8f68f88888c4d10a77ea267f06ae71
abuccts <abuccts@gmail.com>
2015-12-31 11:36:08 +0800

    update

--- js/widget.js
+++ js/widget.js
@@ -82,7 +82,7 @@ function ExtendedCarousel(rootEle, d, cols, param)
 {
   $.each(d, function(i, field) {
     var lightSliderId = 'lightSlider' + i;
-    rootEle.append('<div class="cascade-item"><ul id=' + lightSliderId + '></ul></div>');
+    rootEle.append('<div class="cascade-item"><h3>descripiton</h3><ul id=' + lightSliderId + '></ul></div>');
     $.each(field, function(j, img) {
       $('#' + lightSliderId).append(
       	'<li data-thumb="' + commonUrls.imgRootUrl + img +
@@ -155,15 +155,17 @@ function PagedTable(rootEle, d, param, clickFunc)
     }
     $tbody.append($tr);
   }
-  $tr = $('<tr></tr>');
-  for (var j = 0; j < cols; j ++) {
-    if (d[i * cols + j] == undefined) {
-      $tr.append('<td></td>');
-    } else {
-      $tr.append('<td>' + d[i * cols + j][1] + '</td>');
+  if ( d[i * cols] != undefined) {
+    $tr = $('<tr></tr>');
+    for (var j = 0; j < cols; j ++) {
+      if (d[i * cols + j] == undefined) {
+        $tr.append('<td></td>');
+      } else {
+        $tr.append('<td>' + d[i * cols + j][1] + '</td>');
+      }
     }
+    $tbody.append($tr);
   }
-  $tbody.append($tr);
   $('#tableData').append($tbody);
   $('#tableData').page({limit: param['rows']});
 }
